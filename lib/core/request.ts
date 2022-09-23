bc7a96e7b7ab2283487ae7499c003b501bc321da
Raxabi <beltrandiaztorres@gmail.com>
2022-09-23 14:54:55 +0200

    [ADD] Request object will be a rewrited object of express request with classes and modern syntax

---
+++ lib/core/request.ts
@@ -0,0 +1,11 @@
+/**
+ * * Rewrited subclass Request from express.js with classes syntax
+ */
+
+'use strict';
+
+import { IncomingMessage } from "node:http"
+
+export class Request extends IncomingMessage {
+    
+}
