3405e4c319802b71672495c773f6ccbcfb16ebfa
Raxabi <beltrandiaztorres@gmail.com>
2022-09-23 13:09:58 +0200

    [ADD] Added 'types' property at contributions section

--- contributors.js
+++ contributors.js
@@ -1,10 +1,18 @@
+import { Halley } from "./lib/core/halley"
+import { HRouter as HalleyRouter } from "./lib/core/router/halley.router"
+import * as RouterTypes from "./lib/core/types/router.types"
+
 let contributors = {
     "main_developers": {
         "Raxabi": {
-            "Contributions": [
-                "Halley.JS Core",
-                "Main Project Committer"
-            ]
+            "Contributions": {
+                "Core": [Halley],
+                "Router": [HalleyRouter],
+                "types": [
+                    [RouterTypes]
+                ],
+                "Main Committer": "Literally Me"
+            }
         }
     }
 }
