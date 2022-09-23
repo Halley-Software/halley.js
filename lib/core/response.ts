c7fb7e9e2a525afe52dd0eaeb4d1820b9f7de119
Raxabi <beltrandiaztorres@gmail.com>
2022-09-23 14:55:45 +0200

    [ADD] Response object will be a rewrited object of express response with classes and modern syntax

---
+++ lib/core/response.ts
@@ -0,0 +1,11 @@
+/**
+ * * Rewrited subclass Response from express.js with classes syntax
+ */
+
+'use strict';
+
+import { ServerResponse } from "node:http"
+
+export class Reply extends ServerResponse {
+
+}
