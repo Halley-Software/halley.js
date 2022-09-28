a9fcb6ca60e36d45a2f0e94f511b0765a91ea1f0
Raxabi <beltrandiaztorres@gmail.com>
2022-09-28 09:25:56 +0000

    [ADD] Halley.js now can distinguish between the routes that we pass with halley.get method, if the visited route dont is in the stack halley dont return anything on the browser

--- lib/core/halley.ts
+++ lib/core/halley.ts
@@ -1,14 +1,23 @@
 /**
  * Coded by Raxabi from Halley Software Open Source Developer Team. HSOSDT
  * 
+ * Can call it Halley Software Developer Team. HSDT, because all that we make is Open Source :D
+ * 
  *  Contributors on root of the project, "/contributors.js"
  * 
  */
 
 /**
- * Halley.JS use any third-party modules, so here are the mentions about there :D
+ * Halley is the main core of the project.
+ * Here all the another classes and objects are grouped and send to the node HTTP server
+ */
+
+/**
+ * Halley.js use and take as reference some third-party modules, so here are the mentions about there :D
  * 
- * Halley.JS use the pino logger https://github.com/pinojs/pino
+ * Halley.js use the pino logger https://github.com/pinojs/pino
+ * 
+ * Halley.js take some things as reference from tinyhttp like properties used at tsconfig.json https://github.com/tinyhttp/tinyhttp
  */
 
 'use strict';
@@ -17,66 +26,119 @@
  * Node.JS dependencies
  */
 
-import { createServer, RequestListener } from "node:http";
+import * as http from "node:http";
 
-import { ok } from "node:assert"
+import { ok } from "node:assert";
 
 /**
  * Halley.JS dependencies
  */
 
-import { HRouter } from "./router/halley.router"
+import { Request } from "./request"
+import { Reply } from "./response"
+
+//* Type Anotations
+import { type HalleyListener, type HalleyEnvironment } from "../types/halley.types"
+import * as RouterTypes from "../types/router.types";
 
 export class Halley {
 
-    private port: number;
     /**
+     * @property The port is used to indicate to Halley where need to listen requests. Its a readonly property
      * 
-     * @param options Is the unique parameter for Halley class and its an object.
-     * 
-     * The properties of options indicate Halley.js how must create the http server or how must work some parts of Halley like the built-in Pino logger
-     * 
-     * @param options.env Indicate Halley.js, among other things, in which port must listen for request. It works only if it's indicated
-     * 
-     * @param options.port Indicate Halley.js where need to listen for entering routes. The default port is 5000
-     * 
-     * @public
      */
-    public constructor(options?: {port?: number, env?: string}) {
-        !options?.port ? this.port = 5000 : this.port = options.port;
-    };
+    public readonly port: number;
 
     /**
+     * The env indicate how is be developed an project
      * 
-     * @param path 
-     * @param requestHandler 
-     * 
-     * @public
+     */
+    private env: HalleyEnvironment;
+
+    /**
+     * @property Is an array that contain all the routes declared with get method
+     */
+    private localRoutes: RouterTypes.Route[] = [];
+
+    /**
+     * @property The property will change rely on the visited url
      */
 
-    public get(path: string, requestHandler: RequestListener) {
+    private response: HalleyListener = (req, res) => {};
 
-        ok(path);
-        ok(requestHandler);
-
-        const router = new HRouter;
-        router.add({path: path, method: "get", requestHandler: requestHandler});
+    public settings = {
+        xPoweredBy: "Halley.js"
     }
 
     /**
      * 
-     * @param middleware Middleware is a function that make extra actions on the running project. Similar to Express
+     * @param options Is the unique parameter for Halley class and it's an literal object.
      * 
-     * @Anotation For now, only works receiving a router instance
+     * The values of every property of options indicate Halley.js how must create the http server or how must work some parts of Halley like the built-in Pino logger (Not implemented yet)
+     * 
+     * @param options.env Indicate to Halley how is be developed an project. If it isn't indicated, Halley will assume that is an development environment
+     * 
+     * @param options.port Indicate to Halley where need to listen for entering routes. The default port is 5000
+     * 
      */
+    public constructor(options?: Partial<{port: number, env: HalleyEnvironment}>) {
+        !options?.port ? this.port = 5000 : this.port = options.port;
+        !options?.env ? this.env = "development" : this.env = options.env;
+    };
 
-    public use(middleware: HRouter) {
-        console.log(middleware)
-        return middleware
+    /**
+     * Push the params to an array with all the routes of the running project
+     * 
+     * @param {string} path The path where the listener will execute
+     * @param {HalleyListener} handler A callback function that will execute when the route is visited
+     * 
+     */
+    public get(path: string, handler: HalleyListener) {
+
+        ok(path);
+        ok(handler);
+
+        if (path[0] !== "/") throw new TypeError("A route must start with '/'!")
+
+        this.localRoutes.push({path: path, method: "GET", handler: handler});
+    };
+
+
+    /**
+     * Iterate over the localRoutes of the actual object
+     * @param routeArray The array that contain the routes
+     * @param index the pattern that want to search
+     * @returns 
+     */
+    private iterateRoutes(routeArray: RouterTypes.Route[], index: string) {
+        const suitableObject = routeArray.find(match => {
+            return match.path === index
+        })
+
+        return suitableObject
+    }
+
+    /**
+     * Matches the gived param with any object of localRoutes and attach the handler of the return object to this.response
+     * 
+     * @param path the url of the route to match
+     * 
+     */
+    private makeSuitable(path: string | undefined) {
+        if (path !== undefined) {
+            const alreadyIterated = this.iterateRoutes(this.localRoutes, path)
+            if (alreadyIterated === undefined) this.response = (req, res) => {}
+            if (alreadyIterated !== undefined) {
+                this.response = alreadyIterated.handler
+            }
+        }
     }
 
     /**
      * Ready method start your application and listen for requests on the indicated port at the constructor
+     * 
+     * Commonly some frameworks indicate the port at a method similar to ready (listen, start...).
+     * In Halley.js is preferable indicate the port at the object constructor, so that the port is part of the class
      * 
      * @example
      *      
@@ -84,20 +146,17 @@ export class Halley {
      *      
      *      // Do stuff with that stuff
      * 
-     *      halley.ready(app);
-     * 
-     * @param {Handler} app A handler that manage the route when a request incoming to that route
-     * 
-     * @Anotation Commonly some frameworks indicate the port at a method similar to ready (listen, start...).
-     * 
-     * In Halley.js is preferable indicate the port at the object constructor, so that the port is part of the class
+     *      halley.ready(`Halley listening on port ${halley.port}`)
      * 
      * @public
      */
-
-    public ready(app: RequestListener): void {
-        createServer(app).listen(this.port);
-        // Show the server listening port and apply cyan color to the port number
-        console.info(`Server listening on port \x1b[36m${this.port}\x1b[0m`);
+    public ready(message?: string, hostname?: string): http.Server {
+        const server = http.createServer();
+        server.on("request", (req: Request, res: Reply) => {
+            this.makeSuitable(req.url)
+            this.response.call(this, req, res)
+        })
+        console.info(message);
+        return server.listen(this.port, hostname = "0.0.0.0" || hostname);
     };
 };
