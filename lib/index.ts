267ea6e0fb332d995327e5cbe57d7fe0a3096b49
Raxabi <beltrandiaztorres@gmail.com>
2022-09-28 09:38:35 +0000

    [ADD] Added file to export all the components of Halley.js

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+// Halley Exports
+export { Halley } from "./core/halley.js"
+
+// Halley Types Exports
+export type { HalleyEnvironment, HalleyListener } from "./types/halley.types.js"
+
+// Halley Router Types Exports
+export { Route, type RouteParam } from "./types/router.types.js"
+
+// Utils Exports
+export { HColors } from "./utils/halley.colors.js"
+
+// Utils Types Exports
+export type { HColorsSelection } from "./types/HColors.types.js"
