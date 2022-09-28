da53d6b6d95144199933438a3ac007a96127bf83
Raxabi <beltrandiaztorres@gmail.com>
2022-09-28 09:43:04 +0000

    [DELETE] Deleted router types, was changeds to halley.types.ts or deleted otherwise, probably will be added later

--- lib/core/types/router.types.ts
+++
@@ -1,9 +0,0 @@
-import { RequestListener } from "node:http"
-
-export interface Route {
-    path: string;
-    method: string;
-    requestHandler: RequestListener;
-};
-
-export type RouteParam = Route | Route[]
