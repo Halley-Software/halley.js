932af26735e002968c114176d50e5226e5ca96a4
Raxabi <beltrandiaztorres@gmail.com>
2022-09-30 00:51:36 +0000

    [EDIT] change extension imports from nothing to .js

--- lib/index.ts
+++ lib/index.ts
@@ -1,14 +1,20 @@
+'use strict';
+
 // Halley Exports
-export { Halley } from "./core/halley.js"
+export { Halley } from "./core/halley";
+
+export { Request } from "./core/request.js";
+
+export {} from "./core/response.js";
 
 // Halley Types Exports
-export type { HalleyEnvironment, HalleyListener } from "./types/halley.types.js"
+export type { HalleyEnvironment, HalleyListener } from "./types/halley.types.js";
 
 // Halley Router Types Exports
-export { Route, type RouteParam } from "./types/router.types.js"
+export { Route, type RouteParam } from "./types/router.types.js";
 
 // Utils Exports
-export { HColors } from "./utils/halley.colors.js"
+export { HColors } from "./utils/halley.colors.js";
 
 // Utils Types Exports
-export type { HColorsSelection } from "./types/HColors.types.js"
+export type { HColorsSelection } from "./types/HColors.types.js";
