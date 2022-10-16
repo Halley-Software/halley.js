a051ad4e369c701412fb2507b0d59869c4e0f578
Raxabi <beltrandiaztorres@gmail.com>
2022-10-16 23:50:38 +0000

    [ADD] Finally the ServerResponse class has been correctly extended as the 'Reply' class, for now, it only has some methos that modify the statusCode, show a message and end the response and a under construction response with html files

--- lib/core/reply.ts
+++ lib/core/reply.ts
@@ -4,10 +4,37 @@
 
 'use strict';
 
+/**
+ * NodeJS dependencies
+ */
+
 import { IncomingMessage, ServerResponse, STATUS_CODES } from "node:http"
 
+import { readFile } from "node:fs/promises"
+
+import type * as ReplyTypes from "../types/Reply.types";
+
 export class Reply<Request extends IncomingMessage = IncomingMessage> extends ServerResponse<Request> {
-    status(statusCode: number): string | undefined {
-        return STATUS_CODES[statusCode]
+    /**
+     * 
+     * @param statusReply The status Code
+     * @returns `this` object
+     */
+    status(statusReply: number): this {
+        if (!STATUS_CODES[statusReply]) throw new Error(
+            `The status code ${statusReply} dont exist in the object 'STATUS_CODES', Provided from Node.js`
+        )
+        this.statusCode = statusReply;
+        return this;
+    }
+
+    public send(body: ReplyTypes.body): this {
+        this.end(body);
+        return this;
+    }
+
+    public sendFile(filePath: string): this {
+        readFile(filePath).then(some => {console.log(some)});
+        return this;
     }
 };
