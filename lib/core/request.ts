db7cedc405f2e8e5f09b693d5f361379303054d0
Raxabi <beltrandiaztorres@gmail.com>
2022-10-16 23:54:31 +0000

    [ADD] Added some methods and properties declaration in the Request class

--- lib/core/request.ts
+++ lib/core/request.ts
@@ -1,9 +1,19 @@
 /**
- * * Rewrited subclass Request from express.js with classes syntax
+ * Rewrited subclass Request from express.js with classes syntax
  */
 
 'use strict';
 
+/**
+ * NodeJS dependencies
+ */
 import { IncomingMessage } from "node:http"
 
-export class Request extends IncomingMessage {}
+export class Request extends IncomingMessage {
+
+    private getBody() {
+
+    }
+
+    public body: any;
+};
