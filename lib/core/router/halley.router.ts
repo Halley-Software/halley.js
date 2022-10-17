55cb4412ac4dd34a439113212df4d188402f8349
Raxabi <beltrandiaztorres@gmail.com>
2022-10-17 00:08:25 +0000

    [EDIT] Now the type of 'incomingRoutes' parameter at 'add' method is the same type but now is an union to an array of routes

--- lib/core/router/halley.router.ts
+++ lib/core/router/halley.router.ts
@@ -17,74 +17,76 @@ import { ok } from "node:assert";
  */
 
 //* Type Anotations
-import { Route, RouteParam } from "../../types/router.types";
+import * as RouterTypes from "../../types/Router.types";
+
+/**
+ * Check if all the params was gived, if not, the running script will stop showing a message about it
+ * Is responsible of convert the method to an uppercase string too, it makes that the method be suitable for NodeJS http.Server
+ * @param {Route} objectRoute A route with path, http method and the respective handler for that route
+ * @returns {Route} The same literalObject to push to the array that contains all the routes
+ */
+function checkParams(objectRoute: RouterTypes.Route): RouterTypes.Route {
+    let { path, method, handler } = objectRoute;
+    // Check if any property wasn't gived
+    ok(path);
+    ok(method);
+    ok(handler);
+
+    // Covering problem that may exist
+    method = method.toUpperCase();
+
+    return objectRoute;
+}
 
 export class HRouter {
 
     /**
      * An array that contain all the routes added with router.add method
      */
-    public readonly routes: Route[] = [];
+    private routes: RouterTypes.Route[] = [];
 
     /**
      * 
      * This is a full-form of declare routes, if you want a more simple way try the short-form declaration, using a Halley instance instead.
      * 
-     * @param incomingRoutes An array or an literal object, if is an array, you can give many routes.
+     * @param {Array | Object} incomingRoutes An array or an literal object, if is an array, you can give many routes.
      * 
-     * Meanwhile, if is an literal object you must use the method as much as routes want add.
+     * @returns The router instance, 'this'
+     * 
+     * Meanwhile, if is a literal object you must use the method as much as routes you want add.
      * 
      * @example
      * // If the param gived is an Array:
-     *      router.add([{
-     *          path: "/",
-     *          method: "get",
-     *          requestHandler: ((req, res) => {
-     *              res.end("<h1>Hello World!</h1>")
-     *          })
-     *      }])
+     * router.add([{
+     *  path: "/",
+     *  method: "get",
+     *  requestHandler: ((req, res) => {
+     *      res.end("<h1>Hello World!</h1>")
+     *  })
+     * }])
      * 
-     * @example
      * // If the gived param is an literal object:
-     *      router.add({
-     *          path: "/",
-     *          method: "get",
-     *          requestHandler: ((req, res) => {
-     *              res.end("<h1>Hello World!</h1>")
-     *          })
-     *      })
+     * router.add({
+     *  path: "/",
+     *  method: "get",
+     *  requestHandler: ((req, res) => {
+     *      res.end("<h1>Hello World!</h1>")
+     *  })
+     * })
      * // With this way you only can add one route every time 'add' method is called
-     * 
-     * @returns The route stack of the class HRouter
-     * 
-     * @public
      */
-
-    public add(incomingRoutes: RouteParam): this {
-
-        function checkParams(objectRoute: Route): Route {
-            let { path, method, handler } = objectRoute;
-            // Check if any property wasn't gived
-            ok(path);
-            ok(method);
-            ok(handler);
-
-            // Covering problem that may exist
-            method = method.toUpperCase();
-
-            return objectRoute;
-        }
+    public add(incomingRoutes: RouterTypes.Route | RouterTypes.Route[]): this {
 
         if (Array.isArray(incomingRoutes)) {
-            incomingRoutes.forEach((routeItem: Route) => {
+            incomingRoutes.forEach((routeItem: RouterTypes.Route) => {
                 this.routes.push(checkParams(routeItem));
             });
         }
 
         else if (Object.getPrototypeOf(incomingRoutes) === Object.prototype) {
             this.routes.push(checkParams(incomingRoutes))
-        };
-        
-        return this;
+        }
+
+        return this
     };
 };
