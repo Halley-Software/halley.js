b6a39ec1dddf23e644d6c2f7383dea5b726a8516
Raxabi <beltrandiaztorres@gmail.com>
2022-10-17 23:46:36 +0000

    [ADD] Added 'body' type that wrapper the primitive and non-primitive datatypes: string, number, boolean, object and Buffer

---
+++ lib/types/Reply.types.ts
@@ -0,0 +1,10 @@
+/**
+ * This file contain all the types that the Reply object need
+ */
+
+'use strict';
+
+/**
+ * A type wrapper for primitive and non-primitive data types
+ */
+export type body = string | number | boolean | object | Buffer;
