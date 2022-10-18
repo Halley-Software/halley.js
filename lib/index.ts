5e36dc0b25c76f153dca544dedc9d12d0a76697e
Raxabi <beltrandiaztorres@gmail.com>
2022-10-18 17:24:25 +0000

    [REMOVED] Removed export of Request types, it dont have any type for now

--- lib/index.ts
+++ lib/index.ts
@@ -17,6 +17,5 @@ export { Reply } from "./core/reply.js";
  */
 export type { HalleyEnvironment, HalleyListener } from "./types/Halley.types.js";
 export { Route } from "./types/Router.types.js";
-export {} from "./types/Request.types.js";
 export type { body } from "./types/Reply.types.js";
 export { HColors } from "./utils/halley.colors.js";
