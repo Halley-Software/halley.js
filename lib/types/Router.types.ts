42fe1a02c29f0181f0bad5326303b512fe4857c7
Raxabi <beltrandiaztorres@gmail.com>
2022-10-18 17:11:40 +0000

    [RENAME - DEPUPLICATED COMMIT] Renamed files to have the first letter in uppercase

---
+++ lib/types/Router.types.ts
@@ -0,0 +1,23 @@
+/**
+ * This file contain all the types that HRouter need
+ */
+
+'use strict';
+
+import { HalleyListener } from "./Halley.types.js";
+
+/**
+ * Route Defines the structure of a route.
+ * 
+ * It consumes a path, mathod and a handler:
+ * Repectively the types are:
+ * * path - `string`
+ * * method - `string`
+ * * Function - `HalleyListeners`
+ */
+
+export interface Route {
+    path: string;
+    method: string;
+    handler: HalleyListener;
+}
