0228c2085e93c0980082766d7016b360442b2545
Raxabi <beltrandiaztorres@gmail.com>
2022-10-20 23:06:41 +0000

    [REMOVE] Deleted all the independent dirs that contains types

--- lib/types/HColors.types.ts
+++
@@ -1,7 +0,0 @@
-/**
- * This file contains all the types that HalleyColors need
- */
-
-'use strict';
-
-export type HColorsSelection = "cyan";
--- lib/types/Request.types.ts
+++
@@ -1,5 +0,0 @@
-/**
- * This file contain all the types that the Request object need
- */
-
-'use strict';
