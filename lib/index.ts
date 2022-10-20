439ae5612ad60094114f6a197aae42dd7006a052
Raxabi <beltrandiaztorres@gmail.com>
2022-10-20 23:07:17 +0000

    [MODIFY] Modified path of exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,13 @@
-/**
- * Export the Halley project
- */
+// Exports to send it to npm
 
 'use strict';
 
-/**
- * Core Componentes of Halley
- */
 export { Halley } from "./core/halley.js";
-export { HRouter } from "./core/router/halley.router.js"
 export { Request } from "./core/request.js";
 export { Reply } from "./core/reply.js";
+export { HRouter } from "./core/router/halley.router.js";
+export { HColors } from "./utils/halley.colors.js"
 
-/**
- * Core types of Halley
- */
-export type { HalleyEnvironment, HalleyListener } from "./types/Halley.types.js";
-export { Route } from "./types/Router.types.js";
-export type { body } from "./types/Reply.types.js";
-export { HColors } from "./utils/halley.colors.js";
+export type { HalleyEnvironment, HalleyListener } from "./core/halley.js";
+export type { body } from "./core/reply.js";
+export { Route } from "./core/router/halley.router.js";
