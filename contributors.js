b28351b9f035cfd597e91f2570077dc890ba034c
Raxabi <beltrandiaztorres@gmail.com>
2022-11-12 17:24:50 +0000

    [CHANGE] Changed file strcuture and removed imports to make the file more lightweight

--- contributors.js
+++ contributors.js
@@ -1,24 +1,40 @@
-import { Halley } from "./dist/core/halley.js"
-import * as HalleyTypes from "./dist/types/halley.types.js"
-import { HRouter as HalleyRouter } from "./dist/core/router/halley.router.js"
-import * as RouterTypes from "./dist/types/router.types.js"
-import { HColors } from "./dist/utils/halley.colors.js"
-import * as HColorsTypes from "./dist/types/HColors.types.js"
-
 let contributors = {
     "main_developers": {
         "Raxabi": {
-            "Contributions": {
-                "Core": [Halley],
-                "Router": [HalleyRouter],
-                "HColors": [HColors],
-                "types": [
-                    [HalleyTypes],
-                    [RouterTypes],
-                    [HColorsTypes]
-                ],
-                "Main Committer": "Literally Me"
-            }
+            Contributions: [
+                Core = {
+                    "Halley": [
+                        "Halley Class",
+                        Types = [
+                            "Halley Listener",
+                            "Halley Environment"
+                        ]
+                    ],
+                    "Request": [
+                        "Request Class",
+                        Types = [
+                            ""
+                        ]
+                    ],
+                    "Reply": [
+                        "Reply Class",
+                        Types = [
+                            "body",
+                        ]
+                    ],
+                    "Halley Router": [
+                        "HRouter Class",
+                        Types = [
+                            "Route"
+                        ]
+                    ]
+                },
+                Utils = {
+                    "Halley Colors": [
+                        "HColors Class"
+                    ]
+                }
+            ]
         }
     }
 }
