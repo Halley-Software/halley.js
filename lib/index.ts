4fb01004950b2ecd23a8b9ac95fb2dcb336fbf59
Raxabi <beltrandiaztorres@gmail.com>
2022-11-12 17:38:17 +0000

    [ADD] Added Comment at the start of the file, [REMOVED] Removed some useless (for now) exports [IMPORT-FIX] Route interface now is imported from halley.ts file

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,6 @@
-// Exports to send it to npm
+/**
+ * Original author: Raxabi <openhalleysoftware@gmail.com>
+ */
 
 'use strict';
 
@@ -6,8 +8,5 @@ export { Halley } from "./core/halley.js";
 export { Request } from "./core/request.js";
 export { Reply } from "./core/reply.js";
 export { HRouter } from "./core/router/halley.router.js";
-export { HColors } from "./utils/halley.colors.js"
 
-export type { HalleyEnvironment, HalleyListener } from "./core/halley.js";
-export type { body } from "./core/reply.js";
-export { Route } from "./core/router/halley.router.js";
+export type { HalleyEnvironment, HalleyListener, Route } from "./core/halley.js";
