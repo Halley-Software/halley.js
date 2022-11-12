5268bb51a92af2b127510a7b561203fefbb95f9b
Raxabi <beltrandiaztorres@gmail.com>
2022-11-12 19:20:43 +0000

    [ADD] testing res object

--- test/index.js
+++ test/index.js
@@ -4,11 +4,10 @@ const PORT = 5000 || process.env.PORT;
 
 const halley = new Halley({
     port: PORT,
-    env: "development"
 })
 
 halley.get("/", (req, res) => {
-    res.send("<h1>Hello World!</h1>")
+    res.
 })
 
 halley.ready(halley.port)
