36a3892b6799da290d0061e29fd80e506a1eab07
Raxabi <beltrandiaztorres@gmail.com>
2022-11-12 18:17:14 +0000

    [REMOVE] Removed HRouter import

--- lib/index.ts
+++ lib/index.ts
@@ -7,6 +7,4 @@
 export { Halley } from "./core/halley.js";
 export { Request } from "./core/request.js";
 export { Reply } from "./core/reply.js";
-export { HRouter } from "./core/router/halley.router.js";
-
 export type { HalleyEnvironment, HalleyListener, Route } from "./core/halley.js";
