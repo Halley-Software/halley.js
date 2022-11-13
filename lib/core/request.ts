9e8a83301bae794e0edb24135b3fe83483b751e5
Raxabi <beltrandiaztorres@gmail.com>
2022-11-13 23:59:14 +0000

    [ADD] Added method to get body

--- lib/core/request.ts
+++ lib/core/request.ts
@@ -7,14 +7,23 @@
 /**
  * Node.JS dependencies
  */
-import { IncomingMessage } from "node:http"
+import { IncomingMessage } from "node:http";
 
 // It is a good practise to import objects, classes, etc... from the globalThis object
 const { Buffer } = globalThis;
 
 export class Request extends IncomingMessage {
     
-    public get body() {
-        return
+    public body: Buffer[];
+
+    private getBodyInfo(): void {
+        let data: Buffer[] = [];
+
+        this.on("data", (chunk) => {
+            data.push(chunk);
+        }).on("end", () => {
+            Buffer.concat(data);
+        })
+        this.body = data;
     }
-};
+}
