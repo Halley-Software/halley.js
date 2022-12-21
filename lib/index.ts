935893c6587557142edbe6803ab07371668f75db
Raxabi <beltrandiaztorres@gmail.com>
2022-12-22 00:40:08 +0100

    [IMPORTS-FIX] 'Route' interface and 'HRouter' class now are imported from 'halley.router.ts

--- lib/index.ts
+++ lib/index.ts
@@ -4,7 +4,8 @@
 
 'use strict';
 
-export { Halley, Route } from "./core/halley.js";
+export { Halley } from "./core/halley.js";
 export { Request } from "./core/request.js";
 export { Reply } from "./core/reply.js";
 export type { HalleyEnvironment, HalleyListener } from "./core/halley.js";
+export { HRouter, Route } from "./core/router/halley.router.js"
