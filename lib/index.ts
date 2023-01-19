9ea008b33e3618aa5db04c3e1eccb494e81da75f
Raxabi <beltrandiaztorres@gmail.com>
2023-01-19 20:39:17 +0100

    [ADD] Added Halley.js author at start in of index.js

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,7 @@
 /**
- * Original author: Raxabi <openhalleysoftware@gmail.com>
+ * Halley.js 2022 - 2023
+ * 
+ * Original author: Raxabi <openhalleysoftware@gmail.com> at 2022 - 2023
  */
 
 'use strict';
@@ -8,12 +10,12 @@
 export * from "./core/halley.js";
 export * from "./core/request.js";
 export * from "./core/reply.js";
-export * from "./core/router/halley.router.js"
+export * from "./core/router/halley.router.js";
 
 // Types and Interfaces
 import { HalleyEnvironment as HEnvironment, HalleyListener as HListener } from "./core/halley.js";
-import { Route as RouteInterface } from "./core/router/halley.router.js"
+import { Route as RouteInterface } from "./core/router/halley.router.js";
 
 export type HalleyListener = HListener;
 export type HalleyEnvironment = HEnvironment;
-export interface Route extends RouteInterface{};
+export interface Route extends RouteInterface {};
