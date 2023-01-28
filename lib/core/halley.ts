7fdc4db0067d375216d222cc2ad28f597e287767
Raxabi <beltrandiaztorres@gmail.com>
2023-01-28 16:51:08 +0100

    [ADD] Added logger option to Halley class constructor, without utility btw

--- lib/core/halley.ts
+++ lib/core/halley.ts
@@ -128,6 +128,7 @@ export class Halley {
         options: {
             port: number,
             env?: HalleyEnvironment,
+            logger?: true,
             useNodeEnv?: boolean,
         }
     ) {
