b7275c4dab2ae363206533e9c8ce7bb4aa4767c9
Raxabi <92801687+Raxabi@users.noreply.github.com>
2024-04-20 03:21:56 +0200

    [REFACTOR] each error instance is now a static field of his instanced class

--- lib/errors/RouteErrors.ts
+++ lib/errors/RouteErrors.ts
@@ -1,13 +1,13 @@
 /**
  * Exceptions for errors related to routes
  */
+'use strict';
 
-export class HALLEY_ROUTE_ERROR extends Error {
+export default class HALLEY_ROUTE_ERROR extends Error {
     constructor(message: string, name: string) {
         super(message)
-        this.name = name
-        this.message = message
+        super.name = name
     }
-}
 
-export const HALLEY_ROUTE_DO_NOT_START_WITH_SLASH = new HALLEY_ROUTE_ERROR("A route must start with '/'!", "HALLEY_ROUTE_DO_NOT_START_WITH_SLASH");
+    public static HALLEY_ROUTE_DO_NOT_START_WITH_SLASH = new this("A route must start with '/'!", "HALLEY_ROUTE_DO_NOT_START_WITH_SLASH");
+}
