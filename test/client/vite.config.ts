f36cfb84c238cc56c0c13cc3083a96f9dc8da4dd
Raxabi <92801687+Raxabi@users.noreply.github.com>
2024-04-21 12:12:46 +0200

    [TEST] testing bruh

--- test/client/vite.config.ts
+++ test/client/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000
   },
-  publicDir: false,
-  plugins: [react()],
+  plugins: [preact()],
 })
